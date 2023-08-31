ae125ac231ba8c7a31448d7cc6503a7281308802
Matthew Keslin <mkeslin@outlook.com>
2023-08-31 05:58:37 -0500

    Added 2.4.5

---
+++ 2.4.5 Hello!/HelloTester.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class HelloTester
+{
+    public static void main(String[] args)
+    {
+        // Create a Scanner object
+        Scanner input = new Scanner(System.in);
+
+    }
+}
