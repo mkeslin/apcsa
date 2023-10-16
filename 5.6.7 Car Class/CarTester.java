6a78a60799dd9fd83d7bc273d2b6e1ae958430dc
Matthew Keslin <mkeslin@outlook.com>
2023-10-16 06:19:32 -0500

    Added exercise 5.6.7

---
+++ 5.6.7 Car Class/CarTester.java
@@ -0,0 +1,27 @@
+public class CarTester {
+
+    public static void main(String[] args) {
+        //Create a car that gets 20 mpg and has a 15 gallon tank
+        
+        //Fill up the gas tank
+        
+        //Check the miles available
+
+        //Drive 100 miles
+
+        //Check the miles available
+
+        //Add 2 gallons to the gas tank
+
+        //Check the miles available
+        
+        //Try driving more miles than available by driving 1000 miles
+
+        //Drive 200 miles
+
+        //Check how much gas you have left
+
+        //Print total miles driven
+
+    }
+}
