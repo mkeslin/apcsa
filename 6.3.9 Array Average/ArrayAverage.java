8dbb3babe3354ce628f1af1c6d72d4d0228c746e
Matthew Keslin <mkeslin@outlook.com>
2023-10-26 14:00:31 -0500

    Added exercise 6.3.9

---
+++ 6.3.9 Array Average/ArrayAverage.java
@@ -0,0 +1,14 @@
+public class ArrayAverage
+{
+   private int[] values;
+
+   public ArrayAverage(int[] theValues)
+   {
+      values = theValues;
+   }
+
+   public double getAverage()
+   {
+     // your code goes here!
+   }
+}
---
+++ 6.3.9 Array Average/ArrayAverageTester.java
@@ -0,0 +1,9 @@
+public class ArrayAverageTester
+{
+   public static void main(String[] args)
+   {
+     int[] numArray =  {12, 17, 65, 7, 30, 88};
+     
+     // Create an ArrayAverage object and print the result 
+   }
+}
